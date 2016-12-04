0621ea124a99bd5e5091931af01b16c0e9eb70e0
Paul de Rosanbo <paul@rosanbo.com>
2016-12-04 14:17:50 +0100

    Add service file

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# amazon dash button
+# Amazon dash button for mopidy
+
+Go to next track on your mopidy server with the amazon dash button
+
+
+## Install on debian
+
+```bash
+cd /home/paul/git
+git clone https://github.com/polo2ro/dash
+```
+
+Start the service
+
+Warning, the service file contain path to the js files, this must be modified
+to match your username, your repository folder, and the hostname used by
+mopidy.
+
+```bash
+su
+cp dash/systemd/dash.service /etc/systemd/system/
+systemctl start dash.service
+```
+
+Enable for next reboot
+
+```bash
+systemctl enable dash.service
+```
--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-var dash_button = require('node-dash-button');
-var dash = dash_button("34:d2:70:bc:5b:fc", 'eth0'); //address from step above
+'use strict';
+
+const dashButton = require('node-dash-button');
+const Mopidy = require("mopidy");
+
+let dash = dashButton("34:d2:70:bc:5b:fc", 'eth0'); //address from step above
 dash.on("detected", function() {
-    console.log("omg found");
+
+    let host = 'localhost';
+    if (undefined !== process.argv[2]) {
+        host = process.argv[2];
+    }
+
+    let mopidy = new Mopidy({
+        webSocketUrl: 'ws://'+host+':6680/mopidy/ws/',
+        callingConvention: 'by-position-or-by-name'
+    });
+
+    mopidy.on("state:online", function () {
+        mopidy.playback.next();
+    });
 });
